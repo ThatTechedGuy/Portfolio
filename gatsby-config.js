7df4946fae7a151c55ebc127f895892ce6b33f1b
ThatTechedGuy <vgupta463@gmail.com>
2020-12-10 14:04:36 +0530

    Feat: Manifest added, some theme colours and postcss improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,30 @@
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Vishal Gupta`,
+        short_name: `VG`,
+        description:
+          "Product Engineer and Software Developer based in Kolkata, India.",
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#000`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
     "gatsby-plugin-optimize-svgs",
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
         postCssPlugins: [
-          require("autoprefixer")(),
-          require(`postcss-preset-env`)({ stage: 0 }),
+          require(`postcss-preset-env`)({
+            stage: 0,
+            autoprefixer: { grid: true },
+          }),
           require("postcss-pxtorem")({
             minPixelValue: 0, // Minimal pixel value that will be processed
             propList: [], // List of CSS properties that can be changed from px to rem; empty array means that all CSS properties can change from px to rem
